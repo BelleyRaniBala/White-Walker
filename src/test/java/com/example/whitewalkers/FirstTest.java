05dbcedb36295085f38af166d8451ba43ab5a7e5
belleyrani.bala <balabelley@gmail.com>
2018-06-04 18:07:41 +0100

    JUnit5 is working

---
+++ src/test/java/com/example/whitewalkers/FirstTest.java
@@ -0,0 +1,12 @@
+package com.example.whitewalkers;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    public void newMethod () {
+
+        System.out.println("Belley Test");
+    }
+}
